279b351069e7c6150bff61ab78e351e72432b1df
Aylin Santa Cruz Vargas <aylin_santacruz@hotmail.com>
2021-05-23 22:34:26 -0500

    Agregando el flujo contacto

--- plantitas/Controllers/ContactoController.cs
+++ plantitas/Controllers/ContactoController.cs
@@ -1,7 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
 namespace plantitas.Controllers
 {
-    public class ContactoController
+    public class ContactoController :Controller
     {
-        
+        private readonly ILogger<ContactoController> _logger;
+
+   
+
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
