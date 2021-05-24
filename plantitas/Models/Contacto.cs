4ee6033b6a84b1b339142472dcbb88ca34e73033
Aylin Santa Cruz Vargas <aylin_santacruz@hotmail.com>
2021-05-23 21:32:23 -0500

    Agregando telefono

--- plantitas/Models/Contacto.cs
+++ plantitas/Models/Contacto.cs
@@ -4,6 +4,7 @@ namespace plantitas.Models
     {
        public int ID {get; set;} 
        public string Nombre {get; set;} 
-       public string Email {get; set;} 
+       public string Correo {get; set;} 
+       public string Telefono {get; set;} 
     }
 }
